6929cd05c2b3d2b43778321304667577ab52fab1
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-09 17:20:05 +0100

    feat: linked redux to react app, subscribed to store to local storage changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from './store/configureStore';
+import { saveChats, STORAGE_KEY } from './utils/storage';
+import { loadMessages } from './app/pages/chat/slice';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+store.subscribe(() => {
+  const state = store.getState();
+  saveChats(state.chats.messages || []);
+});
+
+window.addEventListener('storage', (event) => {
+  if (event.key !== STORAGE_KEY) {
+    return;
+  }
+
+  const loadedMessages = JSON.parse(event.newValue as string);
+  if (!Array.isArray(loadedMessages)) {
+    return;
+  }
+
+  store.dispatch(loadMessages(loadedMessages));
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
