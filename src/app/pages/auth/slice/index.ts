4c6ff9d7bdb015ee7707c29b65f72e5006ab245b
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-09 17:15:29 +0100

    feat: setup authentication slice to handle state changes

---
+++ src/app/pages/auth/slice/index.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { AccountState } from './types';
+
+// Define the initial state using that type
+const initialState: AccountState = {
+  userName: '',
+  isAuthenticated: false,
+  id: '',
+};
+
+export const accountSlice = createSlice({
+  name: 'counter',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    loginUser: (state, action: PayloadAction<AccountState>) => {
+      state.userName = action.payload.userName;
+      state.id = action.payload.id;
+      state.isAuthenticated = action.payload.isAuthenticated;
+    },
+  },
+});
+
+export const { loginUser } = accountSlice.actions;
+
+export default accountSlice.reducer;
