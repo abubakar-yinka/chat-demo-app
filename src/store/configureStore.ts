83e24d37a893163cad2368840a4af00aefb0b678
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-09 17:18:41 +0100

    chore: added appropriate reducers to redux store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-// ...
+import ChatReducer from '../app/pages/chat/slice';
+import AccountReducer from '../app/pages/auth/slice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    chats: ChatReducer,
+    account: AccountReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
