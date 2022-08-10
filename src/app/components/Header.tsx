6e5c2313dc00d64180b3b0afdc6627dec046972b
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-10 10:07:52 +0100

    chore: remove network dependence for sole offline usage

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Header: React.FC<Props> = ({ userName }) => {
   return (
     <Flex w="100%">
-      <Avatar size="lg" name={userName} src={`https://ui-avatars.com/api/?name=${userName}`}>
+      <Avatar size="lg" name={userName}>
         <AvatarBadge boxSize="1.25em" bg="green.500" />
       </Avatar>
       <Flex flexDirection="column" mx="5" justify="center">
