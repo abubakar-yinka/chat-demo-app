dcaca9c0029f151a716f59b0040d67b6953188b6
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-10 10:17:48 +0100

    feat: implemented show more messages functionality

--- src/app/components/MessageList.tsx
+++ src/app/components/MessageList.tsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import { Avatar, Flex, Text } from '@chakra-ui/react';
 import { Message } from '../pages/chat/slice/types';
+import { MESSAGE_HISTORY_SIZE } from '../pages/chat';
 
 interface Props {
-  messages: Message[];
+  allMessages: Message[];
+  visibleMessages?: Message[];
   userName: string;
+  clicksOnLoadMore: number;
+  setClicksOnLoadMore: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const MessageList: React.FC<Props> = ({ messages, userName }) => {
+const MessageList: React.FC<Props> = ({
+  allMessages,
+  visibleMessages,
+  userName,
+  clicksOnLoadMore,
+  setClicksOnLoadMore,
+}) => {
   const AlwaysScrollToBottom = () => {
     const elementRef = useRef() as React.LegacyRef<HTMLDivElement> | undefined;
     useEffect(() => ((elementRef as any).current as any).scrollIntoView());
@@ -16,30 +26,35 @@ const MessageList: React.FC<Props> = ({ messages, userName }) => {
 
   return (
     <Flex w="100%" h="80%" overflowY="scroll" flexDirection="column" p="3">
-      {messages.map((message, index) => {
-        if (message.author.toLowerCase() === userName.toLowerCase()) {
-          return (
-            <Flex key={index} w="100%" justify="flex-end">
-              <Flex bg="black" color="white" minW="100px" maxW="350px" my="1" p="3">
-                <Text>{message.messageText}</Text>
+      {allMessages.length > MESSAGE_HISTORY_SIZE * (clicksOnLoadMore + 1) && (
+        <button onClick={() => setClicksOnLoadMore(clicksOnLoadMore + 1)}> Load More </button>
+      )}
+
+      {visibleMessages &&
+        visibleMessages.map((message, index) => {
+          if (message.author.toLowerCase() === userName.toLowerCase()) {
+            return (
+              <Flex key={index} w="100%" justify="flex-end">
+                <Flex bg="black" color="white" minW="100px" maxW="350px" my="1" p="3">
+                  <Text>{message.messageText}</Text>
+                </Flex>
               </Flex>
-            </Flex>
-          );
-        } else {
-          return (
-            <Flex key={index} w="100%">
-              <Avatar
-                name={message.author}
-                src={`https://ui-avatars.com/api/?name=${message.author}`}
-                bg="blue.300"
-              ></Avatar>
-              <Flex bg="gray.100" color="black" minW="100px" maxW="350px" my="1" p="3">
-                <Text>{message.messageText}</Text>
+            );
+          } else {
+            return (
+              <Flex key={index} w="100%">
+                <Avatar
+                  name={message.author}
+                  // src={`https://ui-avatars.com/api/?name=${message.author}`}
+                  bg="blue.300"
+                ></Avatar>
+                <Flex bg="gray.100" color="black" minW="100px" maxW="350px" my="1" p="3">
+                  <Text>{message.messageText}</Text>
+                </Flex>
               </Flex>
-            </Flex>
-          );
-        }
-      })}
+            );
+          }
+        })}
       <AlwaysScrollToBottom />
     </Flex>
   );
--- src/app/pages/chat/index.tsx
+++ src/app/pages/chat/index.tsx
@@ -9,16 +9,27 @@ import { selectMessages } from './slice/selectors';
 import { selectUserId } from '../auth/slice/selectors';
 import { useDispatch } from 'react-redux';
 import { sendMessage } from './slice';
+import { Message } from './slice/types';
 
 interface Props {
   userName: string;
 }
 
+export const MESSAGE_HISTORY_SIZE = 25;
+
 const Chat: React.FC<Props> = ({ userName }) => {
   const dispatch = useDispatch();
   const messages = useSelector(selectMessages);
   const userId = useSelector(selectUserId);
-  const [inputMessage, setInputMessage] = React.useState('');
+  const [inputMessage, setInputMessage] = React.useState<string>('');
+  const [clicksOnLoadMore, setClicksOnLoadMore] = React.useState<number>(0);
+  const [visibleMessages, setVisibleMessages] = React.useState<Message[]>();
+
+  React.useEffect(() => {
+    const computedLength = MESSAGE_HISTORY_SIZE * (clicksOnLoadMore + 1);
+    const desiredMessages = messages.slice(-computedLength);
+    setVisibleMessages(desiredMessages);
+  }, [messages, clicksOnLoadMore]);
 
   const handleSendMessage = () => {
     if (!inputMessage.trim().length) {
@@ -32,7 +43,13 @@ const Chat: React.FC<Props> = ({ userName }) => {
       <Flex w={['100%', '100%', '70%']} h="90%" flexDir="column">
         <Header userName={userName} />
         <Divider />
-        <MessageList messages={messages} userName={userName} />
+        <MessageList
+          allMessages={messages}
+          visibleMessages={visibleMessages}
+          userName={userName}
+          clicksOnLoadMore={clicksOnLoadMore}
+          setClicksOnLoadMore={setClicksOnLoadMore}
+        />
         <Divider />
         <MessageInput
           inputMessage={inputMessage}
